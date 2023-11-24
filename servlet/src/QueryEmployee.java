3bb4f0d4771ceb21aa1a15746cf00713e3f1d895
hdw-Qi <2799213883@qq.com>
2023-11-24 15:17:50 +0800

    创建QueryEmployee类

---
+++ servlet/src/QueryEmployee.java
@@ -0,0 +1,11 @@
+/**
+ * @projectName: git
+ * @package: PACKAGE_NAME
+ * @className: QueryEmployee
+ * @author: 胡代伟
+ * @description: 查询员工列表
+ * @date: 2023/11/24 15:16
+ * @version: 1.0
+ */
+public class QueryEmployee {
+}
