ba0b7834d802cea37f2fae9f3d05bdbea0942d6c
hdw-Qi <2799213883@qq.com>
2023-12-26 14:29:53 +0800

    文件上传

--- springMVC/src/main/java/controller/HelloController.java
+++ springMVC/src/main/java/controller/HelloController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -38,8 +41,40 @@ public class HelloController {
 
 
     //测试一个文件上传
+    //步骤
+    //1、导入文件上传依赖
+    //2、springmvc配置文件配置文件上传解析器
+    @PostMapping("/uploadImg")
+    @ResponseBody
+    public String uploadImg(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        System.out.println(originalFilename);
+        try {
+            file.transferTo(new File("D:\\Java_GuoXinAn\\git\\springMVC\\src\\main\\webapp\\uploadImg\\"+originalFilename));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return originalFilename;
 
-    //测试多个文件上传
+    }
+
+    //测试多个文件上传(前端的多个文件的name值要相同)
+    @PostMapping("/uploadManyImg")
+    @ResponseBody
+    public int uploadManyImg(MultipartFile[] file) {
+        for (MultipartFile multipartFile : file) {
+            String originalFilename = multipartFile.getOriginalFilename();
+            System.out.println(originalFilename);
+            try {
+                multipartFile.transferTo(new File("D:\\Java_GuoXinAn\\git\\springMVC\\src\\main\\webapp\\uploadImg\\"+originalFilename));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return file.length;
+    }
+
 
 
 }
