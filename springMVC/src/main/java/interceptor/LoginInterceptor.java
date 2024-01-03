5cc3dd2b8fd5b774e1bcb4b7ac504d7ac376aefb
hdw-QI <120390534+hdw-QI@users.noreply.github.com>
2024-01-03 17:47:26 +0800

    是否登陆拦截器

---
+++ springMVC/src/main/java/interceptor/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package interceptor;
+
+import exception.MyException;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 胡代伟
+ * @description 是否登陆拦截器
+ * @date 2024-01-03 16:47
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null) {
+            throw new MyException("请先登录！");
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
