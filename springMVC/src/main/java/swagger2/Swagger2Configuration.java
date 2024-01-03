c9ed420afb665ebc00ea2432ba2e1cc8704ab9d0
hdw-QI <120390534+hdw-QI@users.noreply.github.com>
2024-01-03 16:45:24 +0800

    swagger携带token

--- springMVC/src/main/java/swagger2/Swagger2Configuration.java
+++ springMVC/src/main/java/swagger2/Swagger2Configuration.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author 胡代伟
@@ -26,7 +30,10 @@ public class Swagger2Configuration {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                // swagger携带token。效果：页面中多了一个 Authorize 按钮
+                .securityContexts(Arrays.asList(securityContexts()))
+                .securitySchemes(Arrays.asList(securitySchemes()));
     }
 
     //基本信息的配置，信息会在api文档上显示
@@ -36,6 +43,25 @@ public class Swagger2Configuration {
                 .description("xx相关接口的文档")
                 .termsOfServiceUrl("http://www.baidu.com/")
                 .version("1.0")
+                .build()
+                ;
+    }
+
+    private SecurityScheme securitySchemes() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContexts() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("xxx", "描述信息");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
     }
 }
